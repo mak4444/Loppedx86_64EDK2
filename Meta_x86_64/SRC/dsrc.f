d3560fd1d1af8c3e4b755998e2429f9d60176a54
mak4444 <fpauk@mail.ru>
2026-04-24 17:25:05 +0300

    Update dsrc.f

--- Meta_x86_64/SRC/dsrc.f
+++ Meta_x86_64/SRC/dsrc.f
@@ -3,8 +3,7 @@
 
  REQUIRE forth ~mak/lib/caseins.f
 \+ CASE-INS  CASE-INS ON
-
-REQUIRE [AGAIN] ~mak/lib/twopass.f 
+\- IMAGE-BEGIN ' IMGLIT VALUE IMAGE-BEGIN
 
 CREATE SEG_DT
   0 , 0 ,
@@ -23,20 +22,15 @@ CONSTANT SEG_DT_END
 ;
 
 : >SEG_DT ( madr tadr -- )
-\ ." SEG_DT00=" .s cr
  SEG_DT
-\ dup 44 dump
  BEGIN  CELL+ CELL+  2DUP @ U<
  UNTIL  \  madr tadr dtadr
-\ ." SEG_DT30=" .s cr
  DUP
  DUP CELL+ CELL+ SEG_DT_END OVER - CMOVE>
  >R
  TUCK - SWAP
  R> 2!
 ;
-
-0 VALUE LOW_LIM
 
 WARNING OFF
 Meta_x86_64/mhead0.f
@@ -48,17 +42,5 @@ REQUIRE TC_?LIMIT ~mak/lib/THERE/STAT.F
 Meta_x86_64/mhead.f
 
  MEM_MODE
-
-\- W>S : W>S ( w -- n )  0xFFFF AND 0x8000 XOR 0x8000 - ;
-
-[IFNDEF] H.R
-: H.R           ( n1 n2 -- )    \ display n1 as a hex number right
-                                \ justified in a field of n2 characters
-                BASE @ >R HEX >R
-                0 <# #S #> R> OVER - 0 MAX SPACES TYPE
-                R> BASE ! ;
-[THEN]
-
-REQUIRE T-START	Meta_x86_64/SRC/tc.f 
-
-
+ 
+ Meta_x86_64/SRC/tc.f 
