4f2c073c4b75881accf15ff882e273349ec4b8d9
mak4444 <fpauk@mail.ru>
2026-05-06 14:45:25 +0300

    Create AutoGen.h

---
+++ OVMF/ForthPkg/Forth/ff/AutoGen.h
@@ -0,0 +1,39 @@
+
+#ifndef _AUTOGENH_EEC25BDC_67F2_4D95_B1D5_F81B2039D11D
+#define _AUTOGENH_EEC25BDC_67F2_4D95_B1D5_F81B2039D11D
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Uefi.h>
+#include <Library/PcdLib.h>
+
+// Guids
+
+// Protocols
+
+// Definition of SkuId Array
+
+// Definition of PCDs used in this module
+
+// Definition of PCDs used in libraries is in AutoGen.c
+
+
+EFI_STATUS
+EFIAPI
+BootManagerMenuEntry (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  );
+
+
+
+//#include "BootManagerMenuAppStrDefs.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
